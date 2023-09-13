83e0a1e1422a914411c4e35450bcb5ac061d3e74
henry <terione12@gmail.com>
2023-09-12 20:52:10 -0600

    Test

--- src/Othello.java
+++ src/Othello.java
@@ -1,22 +1,15 @@
 import cs251.lab2.OthelloGUI;
 import cs251.lab2.OthelloInterface;
+import java.util.Random;
 
-/**
- * The type Othello.
- */
 public class Othello implements OthelloInterface {
     private StringBuilder boardString;
     private Piece currentPlayer;
     private Piece[][] boardState;
+    private boolean playerTurn = true;
 
-    /**
-     * Instantiates a new Othello.
-     */
     public Othello() {
-        // Initialize the boardState array with the default size.
         boardState = new Piece[DEFAULT_SIZE][DEFAULT_SIZE];
-
-        // Initialize all elements to Piece.EMPTY
         for (int i = 0; i < DEFAULT_SIZE; i++) {
             for (int j = 0; j < DEFAULT_SIZE; j++) {
                 boardState[i][j] = Piece.EMPTY;
@@ -24,11 +17,6 @@ public class Othello implements OthelloInterface {
         }
     }
 
-    /**
-     * The entry point of application.
-     *
-     * @param args the input arguments
-     */
     public static void main(String[] args) {
         Othello game = new Othello();
         if (args.length > 0) {
@@ -37,146 +25,102 @@ public class Othello implements OthelloInterface {
         OthelloGUI.showGUI(game);
     }
 
-    /**
-     * @return
-     */
     @Override
     public int getSize() {
         return DEFAULT_SIZE;
     }
 
-    /**
-     * Initialize the game board, starting pieces, and the initial player's turn.
-     */
     @Override
     public void initGame() {
-        System.out.println("initGame method called");
         boardString = new StringBuilder();
-        currentPlayer = Piece.DARK; // Set the initial player's turn to black.
-
-        // Initialize the boardState array.
+        currentPlayer = Piece.DARK;
         boardState = new Piece[DEFAULT_SIZE][DEFAULT_SIZE];
-
-        // Initialize the boardState array and boardString.
         for (int i = 0; i < DEFAULT_SIZE; i++) {
             for (int j = 0; j < DEFAULT_SIZE; j++) {
                 if ((i == DEFAULT_SIZE / 2 - 1 && j == DEFAULT_SIZE / 2 - 1) ||
                         (i == DEFAULT_SIZE / 2 && j == DEFAULT_SIZE / 2)) {
-                    // Place initial white pieces.
                     boardState[i][j] = Piece.LIGHT;
                     boardString.append(Piece.LIGHT.toChar());
-                    System.out.println("Placed LIGHT piece at (" + i + ", " + j + ")");
                 } else if ((i == DEFAULT_SIZE / 2 - 1 && j == DEFAULT_SIZE / 2) ||
                         (i == DEFAULT_SIZE / 2 && j == DEFAULT_SIZE / 2 - 1)) {
-                    // Place initial black pieces.
                     boardState[i][j] = Piece.DARK;
                     boardString.append(Piece.DARK.toChar());
-                    System.out.println("Placed DARK piece at (" + i + ", " + j + ")");
                 } else {
-                    // Place empty cells.
                     boardState[i][j] = Piece.EMPTY;
-                    boardString.append(Piece.EMPTY.toChar()); // Append '.' for an empty cell.
-                    System.out.println("Placed EMPTY cell at (" + i + ", " + j + ")");
-                }
-
-                if (j < DEFAULT_SIZE - 1) {
-                    boardString.append(" ");
-                }
-
-                if (j == DEFAULT_SIZE - 1) {
-                    boardString.append("\n"); // Append newline at the end of each row.
+                    boardString.append(Piece.EMPTY.toChar());
                 }
             }
-        }
-        System.out.println("Initialized boardString:");
-        System.out.println(boardString.toString());
-        System.out.println("Initialized boardState:");
-        for (int i = 0; i < DEFAULT_SIZE; i++) {
-            for (int j = 0; j < DEFAULT_SIZE; j++) {
-                System.out.print(boardState[i][j].toChar() + " ");
-            }
-            System.out.println();
         }
     }
 
-    /**
-     * @return
-     */
     @Override
     public String getBoardString() {
-        return boardString.toString();
+        StringBuilder formattedBoard = new StringBuilder();
+        for (int i = 0; i < DEFAULT_SIZE; i++) {
+            for (int j = 0; j < DEFAULT_SIZE; j++) {
+                formattedBoard.append(boardState[i][j].toChar());
+            }
+            formattedBoard.append("\n");
+        }
+        return formattedBoard.toString();
     }
 
-    /**
-     * @return
-     */
     @Override
     public Piece getCurrentTurn() {
         return currentPlayer;
     }
 
-    /**
-     * Handle a player's click at the specified coordinates.
-     *
-     * @param x The x-coordinate of the clicked cell.
-     * @param y The y-coordinate of the clicked cell.
-     * @return The game result after the move.
-     */
     @Override
     public Result handleClickAt(int x, int y) {
-        System.out.println("handleClickAt method called");
-        int cellSize = getSize() * 75 / DEFAULT_SIZE; // Calculate cell size dynamically.
+        if (isLegal(x, y)) {
+            int[][] directions = {
+                    {-1, -1}, {-1, 0}, {-1, 1},
+                    {0, -1}, /* Center */ {0, 1},
+                    {1, -1}, {1, 0}, {1, 1}
+            };
 
-        // Calculate clickedCellX and clickedCellY based on cellSize.
-        int clickedCellX = x / cellSize;
-        int clickedCellY = y / cellSize;
+            for (int[] dir : directions) {
+                int dx = dir[0];
+                int dy = dir[1];
+                int posX = x + dx;
+                int posY = y + dy;
 
-        System.out.println("Clicked at (" + x + ", " + y + ")");
-        System.out.println("Clicked cell: (" + clickedCellX + ", " + clickedCellY + ")");
-        System.out.println("Current player: " + getCurrentTurn());
-
-        // Rest of your handleClickAt method...
-
-        if (isLegal(clickedCellX, clickedCellY)) {
-            // Update the board state with the current player's piece.
-            boardState[clickedCellX][clickedCellY] = getCurrentTurn();
-
-            // Update the corresponding character in boardString.
-            boardString.setCharAt(clickedCellX * (DEFAULT_SIZE + 1) + clickedCellY, getCurrentTurn().toChar());
-
-            // Rest of your handleClickAt method...
-
-            System.out.println("Updated boardState:");
-            for (int i = 0; i < DEFAULT_SIZE; i++) {
-                for (int j = 0; j < DEFAULT_SIZE; j++) {
-                    System.out.print(boardState[i][j].toChar() + " ");
+                if (posX < 0 || posX >= DEFAULT_SIZE || posY < 0 || posY >= DEFAULT_SIZE || boardState[posX][posY] != (currentPlayer == Piece.DARK ? Piece.LIGHT : Piece.DARK)) {
+                    continue;
                 }
-                System.out.println();
+
+                while (posX >= 0 && posX < DEFAULT_SIZE && posY >= 0 && posY < DEFAULT_SIZE && boardState[posX][posY] == (currentPlayer == Piece.DARK ? Piece.LIGHT : Piece.DARK)) {
+                    posX += dx;
+                    posY += dy;
+                }
+
+                if (posX >= 0 && posX < DEFAULT_SIZE && posY >= 0 && posY < DEFAULT_SIZE && boardState[posX][posY] == currentPlayer) {
+                    int flipX = x;
+                    int flipY = y;
+                    while (flipX != posX || flipY != posY) {
+                        boardState[flipX][flipY] = currentPlayer;
+                        boardString.setCharAt(flipX * DEFAULT_SIZE + flipY, currentPlayer.toChar());
+                        flipX += dx;
+                        flipY += dy;
+                    }
+                }
             }
 
-            System.out.println("Updated boardString:");
-            System.out.println(boardString.toString());
-
+            boardState[x][y] = getCurrentTurn();
+            boardString.setCharAt(x * DEFAULT_SIZE + y, getCurrentTurn().toChar());
+            currentPlayer = (currentPlayer == Piece.DARK) ? Piece.LIGHT : Piece.DARK;
+            playerTurn = false;
             return checkGameOver();
         } else {
-            // Handle an illegal move (e.g., show a message to the player).
-            System.out.println("Illegal move!");
-            return Result.GAME_NOT_OVER; // Modify this according to your game logic.
+            return Result.GAME_NOT_OVER;
         }
     }
 
-
-    /**
-     * Helper method to check if the game is over.
-     *
-     * @return The game result (WHITE_WINS, BLACK_WINS, DRAW, or GAME_NOT_OVER).
-     */
     private Result checkGameOver() {
         int darkCount = 0;
         int lightCount = 0;
         int emptyCount = 0;
 
-        // Count the number of dark, light, and empty cells on the board.
         for (int i = 0; i < DEFAULT_SIZE; i++) {
             for (int j = 0; j < DEFAULT_SIZE; j++) {
                 if (boardState[i][j] == Piece.DARK) {
@@ -190,7 +134,6 @@ public class Othello implements OthelloInterface {
         }
 
         if (darkCount + lightCount == DEFAULT_SIZE * DEFAULT_SIZE || emptyCount == 0) {
-            // The game is over if the board is full or there are no empty cells.
             if (darkCount > lightCount) {
                 return Result.DARK_WINS;
             } else if (lightCount > darkCount) {
@@ -203,21 +146,42 @@ public class Othello implements OthelloInterface {
         return Result.GAME_NOT_OVER;
     }
 
-    /**
-     * @param x
-     * @param y
-     * @return
-     */
     @Override
     public boolean isLegal(int x, int y) {
+        if (x < 0 || x >= DEFAULT_SIZE || y < 0 || y >= DEFAULT_SIZE || boardState[x][y] != Piece.EMPTY) {
+            return false;
+        }
+
+        int[][] directions = {
+                {-1, -1}, {-1, 0}, {-1, 1},
+                {0, -1}, {0, 1},
+                {1, -1}, {1, 0}, {1, 1}
+        };
+
+        for (int[] dir : directions) {
+            int dx = dir[0];
+            int dy = dir[1];
+            int posX = x + dx;
+            int posY = y + dy;
+
+            if (posX < 0 || posX >= DEFAULT_SIZE || posY < 0 || posY >= DEFAULT_SIZE || boardState[posX][posY] != (currentPlayer == Piece.DARK ? Piece.LIGHT : Piece.DARK)) {
+                continue;
+            }
+
+            while (posX >= 0 && posX < DEFAULT_SIZE && posY >= 0 && posY < DEFAULT_SIZE && boardState[posX][posY] == (currentPlayer == Piece.DARK ? Piece.LIGHT : Piece.DARK)) {
+                posX += dx;
+                posY += dy;
+            }
+
+            if (posX >= 0 && posX < DEFAULT_SIZE && posY >= 0 && posY < DEFAULT_SIZE && boardState[posX][posY] == currentPlayer) {
+                return true;
+            }
+        }
+
         return false;
     }
 
-    /**
-     * @param s
-     */
-    @Override
-    public void initComputerPlayer(String s) {
-
+    public void initComputerPlayer(String settings) {
+        // Implementation for initializing the computer player goes here.
     }
 }
